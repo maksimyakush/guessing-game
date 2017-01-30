93dd80bd8dd541094252deb570518cb8819b9606
maksimyakush <maksimo-1988@yandex.ru>
2017-01-30 20:54:13 +0300

    first-commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let middle = Math.ceil((this.min + this.max) / 2);
+        return (middle);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
